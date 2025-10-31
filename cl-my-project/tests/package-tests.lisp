2dbb7b451800f2181b3d57dddb10e5fbe374db81
Nicolas O <noccis@I7413>
2025-10-31 15:51:59 +0100

    Add precision

--- cl-my-project/tests/package-tests.lisp
+++ cl-my-project/tests/package-tests.lisp
@@ -4,6 +4,8 @@
                 double
                 triple
                 ))
+;; instead of 'import-from', you can also refer to functions
+;; in tests files as cl-my-project::double, etc.
 
 (in-package :cl-my-project-tests)
 
